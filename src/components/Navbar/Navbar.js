fa7622e51d83f3dac0eda8d28723d8a911ff4e48
tourhzaoui khalid <111646098+therealkhalid@users.noreply.github.com>
2023-05-17 15:29:22 +0100

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,17 +5,17 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-4 p-3">
         <Link className="navbar-brand" to={'/'}>Music App</Link>
-        <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-            aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="collapsibleNavId">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                 <li className="nav-item active">
-                    <Link className='nav-link' to={'/'}>Acceuil</Link>
+                    <Link className='nav-link' aria-current="page" to={'/'}>Acceuil</Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link" to={'/favo'}>Favories</Link>
+                    <Link className="nav-link"  aria-current="page"to={'/favo'}>Favories</Link>
                 </li>
             </ul>
             
